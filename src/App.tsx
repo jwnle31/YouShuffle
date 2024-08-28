dc10f9a5496a0544176dcce0f53f759159f603b5
Jowoon Lee <jwnle31@gmail.com>
2024-08-28 12:11:58 -0400

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { YTPlaylist } from './components' // Adjust the import path as needed
 import { Navbar, Footer } from './layout'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 
 const App: React.FC = () => (
@@ -8,6 +9,7 @@ const App: React.FC = () => (
     <Navbar />
     <YTPlaylist />
     <Footer />
+    <Analytics />
     <SpeedInsights />
   </>
 )
