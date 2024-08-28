7320bf74413a824c9cb59d3bb73af453ff334f4f
Jowoon Lee <jwnle31@gmail.com>
2024-08-28 11:30:41 -0400

    Fix Vercel SpeedInsights Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { YTPlaylist } from './components' // Adjust the import path as needed
 import { Navbar, Footer } from './layout'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 const App: React.FC = () => (
   <>
     <Navbar />
     <YTPlaylist />
     <Footer />
+    <SpeedInsights />
   </>
 )
 
